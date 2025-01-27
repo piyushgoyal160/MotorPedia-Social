9fec5aa94640e09e18cf66aae0015c084cb27154
PIYUSH GOYAl <piyushgoyal1600@gmail.com>
2025-01-27 22:29:03 +0530

    Create a social handle UI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Front from "./front";
+
+function App() {
+  return <Front />;
+}
+
+export default App;
